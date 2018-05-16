22058ff486c8c3c83fb288c343fb77d7e6604a1c
gregory.dannay@sciencespo.fr <gregory.dannay@sciencespo.fr>
2018-05-16 20:29:42 +0200

    NYC

--- 02-appli-networks/subway/SPR_NYC.py
+++ 02-appli-networks/subway/SPR_NYC.py
@@ -6,15 +6,16 @@ import numpy as np
 
 # Extracting the data
 thepath = os.getcwd()
-arcs = pd.read_csv(thepath + "/Paris/arcs.csv", sep=';', header=None).values
-namesNodes = pd.read_csv(thepath + "/Paris/nodes.csv", sep=',', header=None)
+arcs = pd.read_csv(thepath + "/NYC/arcs.csv", sep=',').values
+namesNodes = pd.read_csv(thepath + "/NYC/nodes.csv", sep=',')
 
 
 # Preparing the data: setting up the graph and weight matrices
 nbNodes = int(np.amax(arcs[:, 0]))
-arcs[:, :-1] = arcs[:, :-1] - 1     # Correcting for the 0 index
+arcs[:, :2] = arcs[:, :2] - 1     # Correcting for the 0 index
 
-nodes = arcs[:, :-1]
+names = namesNodes.iloc[:, 0] + namesNodes.iloc[:, 6]
+nodes = arcs[:, :2]
 weights = arcs[:, 2]
 graph = [nodes[nodes[:, 0] == i, 1] for i in range(nbNodes)]
 
@@ -24,18 +25,19 @@ for idx, i, j in zip(range(len(arcs)), arcs[:, 0], arcs[:, 1]):
 
 
 # Call the algorithm
-originMode = 84 - 1
-destionationNode = 116 - 1
+originMode = 452 - 1
+destionationNode = 471 - 1
 dist, prec = dijkstra(graph, w_matrix, originMode, destionationNode)
 
 
 # Display the solution
-print('Minimum distance from', namesNodes.iloc[originMode, 0], 'to',
-      namesNodes.iloc[destionationNode, 0], '\n', dist[destionationNode])
+print('Minimum distance from', names[originMode], 'to',
+      names[destionationNode], '\n', dist[destionationNode])
 path = destionationNode
 step = 0
+print('\n\nPath:')
 while path != originMode:
-    print(step, namesNodes.iloc[path, 0], '(#%d)' % (path + 1))
+    print(step, names[path], '(#%d)' % (path + 1))
     step += 1
     path = prec[path]
-print(step, namesNodes.iloc[path, 0], '(#%d)' % (path + 1))
+print(step, names[path], '(#%d)' % (path + 1))
