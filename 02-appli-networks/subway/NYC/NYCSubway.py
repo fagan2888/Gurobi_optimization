2de99e1440241d32d53930ffe0b718696ba88ce4
gregory.dannay@sciencespo.fr <gregory.dannay@sciencespo.fr>
2018-05-20 14:26:07 +0200

    NY

--- 02-appli-networks/subway/NYC/NYCSubway.py
+++ 02-appli-networks/subway/NYC/NYCSubway.py
@@ -1,31 +1,31 @@
 import pandas as pd
-import gurobipy as grb
 import os
 import numpy as np
-import time
+import gurobipy as grb
+from collections import Counter
 from igraph import *
 
 
 # Extracting the data
 thepath = os.getcwd()
-arcsData = pd.read_csv(thepath + "/arcs.csv", sep=';', header=None)
-namesNodes = pd.read_csv(thepath + "/nodes.csv", sep=',', header=None)
+arcsData = pd.read_csv(thepath + "/arcs.csv", sep=',').drop_duplicates(subset=['from_stop_nb','to_stop_nb']).values
+namesNodes = pd.read_csv(thepath + "/nodes.csv", sep=',')
 
 
-# Preparing the data: nodes and weight matrices
-nbNodes = arcsData.iloc[:, 0].max()
-arcsList = [(i, j) for i, j in zip(arcsData.iloc[:, 0], arcsData.iloc[:, 1])]
-weights = arcsData.iloc[:, 2]
-originNode = 84
-destinationNode = 116
+# Preparing the data: setting up the graph and weight matrices
+nbNodes = int(np.amax(arcsData[:, 0]))
+names = namesNodes.iloc[:, 0] + ' ' + namesNodes.iloc[:, 6]
+arcsList = [(i, j) for i, j in zip(arcsData[:, 0], arcsData[:, 1])]
+weights = arcsData[:, 2]
+originNode = 452
+destinationNode = 471
 
 # Setting up the model
-m = grb.Model('subway')
+m = grb.Model('Subway')
 arcs = m.addVars(arcsList, obj=weights, name='arcs')
-m.addConstrs((arcs.sum('*', station) - arcs.sum(station, '*') == 0 for station in range(nbNodes)
-              if station not in [originNode, destinationNode]), name='constr')
-m.addConstr(arcs.sum('*', originNode) - arcs.sum(originNode, '*') == 1, name='constr')
-m.addConstr(arcs.sum('*', destinationNode) - arcs.sum(destinationNode, '*') == -1, name='constr')
+m.addConstrs((arcs.sum('*', station) - arcs.sum(station, '*') == 0 for station in range(nbNodes) if station not in [originNode, destinationNode]), name='Constr')
+m.addConstr(arcs.sum('*', originNode) - arcs.sum(originNode, '*') == 1, name='Constr')
+m.addConstr(arcs.sum('*', destinationNode) - arcs.sum(destinationNode, '*') == -1, name='Constr')
 
 m.optimize()
 
@@ -35,21 +35,21 @@ pathList = []
 step = 1
 if m.status == grb.GRB.Status.OPTIMAL:
     print('***Optimal solution***')
-    print('Minimum distance from', namesNodes.iloc[originNode - 1, 0], 'to',
-          namesNodes.iloc[destinationNode - 1, 0], '\n', m.objVal)
-    print('0 :', namesNodes.iloc[originNode - 1, 0], '(#%d)' % originNode)
+    print('Minimum distance from', names[originNode - 1], 'to',
+          names[destinationNode - 1], '\n', m.objVal)
+    print('0 :', names[originNode - 1], '(#%d)' % originNode)
     solution = m.getAttr('x', arcs)
     while path != destinationNode:
         for arc in arcsList:
             if arc[1] == path and solution[arc] == 1:
-                print(step, ':', namesNodes.iloc[arc[0] - 1, 0], '(#%d)' % (arc[0]))
+                print(step, ':', names[arc[0] - 1], '(#%d)' % arc[0])
                 path = arc[0]
                 pathList.append(path)
                 step += 1
 
 # Setting up the graph and the visual style
 g = Graph()
-edges = [(i, j) for i, j in zip(arcsData.iloc[:, 0], arcsData.iloc[:, 1]) if i <= j]
+edges = [(i, j) for idx, (i, j) in enumerate(zip(arcsData[:, 0], arcsData[:, 1])) if (j, i) not in arcsList[:idx]]
 coordinates = [(i, -j) for i, j in zip(namesNodes.iloc[:, 2], namesNodes.iloc[:, 3])]
 g.add_vertices(nbNodes + 1)
 g.add_edges(edges)
@@ -64,7 +64,7 @@ visual_style["vertex_size"] = [10 for i in range(nbNodes)]
 visual_style["vertex_size"][originNode - 1] = 40
 visual_style["vertex_size"][destinationNode - 1] = 40
 visual_style["edge_arrow_size"] = 0
-visual_style["margin"] = (-200, -300, -700, -1000)
+visual_style["margin"] = (-500, -500, -500, -500)
 visual_style["bbox"] = (1100, 700)
 visual_style["vertex_label"] = [None for i in range(nbNodes)]
 visual_style["vertex_label"][originNode - 1] = namesNodes.iloc[originNode - 1, 0]
@@ -72,14 +72,14 @@ visual_style["vertex_label"][destinationNode - 1] = namesNodes.iloc[destinationN
 plot(g, **visual_style)
 
 # Show the optimal path with iGraph
+
 path = originNode
 if m.status == grb.GRB.Status.OPTIMAL:
     while path != destinationNode:
         path = pathList.pop(0)
         visual_style["vertex_size"][path - 1] = 40
-        visual_style["vertex_label"][path - 1] = namesNodes.iloc[path - 1, 0]
+        visual_style["vertex_label"][path- 1] = namesNodes.iloc[path - 1, 0]
         plot(g, **visual_style)
-        visual_style["vertex_size"][path - 1] = 10
-        visual_style["vertex_label"][path - 1] = None
+        visual_style["vertex_size"][path- 1] = 10
+        visual_style["vertex_label"][path- 1] = None
         #time.sleep(0.5)
-
