055117aa82869953a61239d7686b7f3057f0ca35
gregory.dannay@sciencespo.fr <gregory.dannay@sciencespo.fr>
2018-05-20 11:15:12 +0200

    Corr

--- 02-appli-networks/subway/SPR_NYC.py
+++ 02-appli-networks/subway/SPR_NYC.py
@@ -13,12 +13,10 @@ namesNodes = pd.read_csv(thepath + "/NYC/nodes.csv", sep=',')
 # Preparing the data: setting up the graph and weight matrices
 nbNodes = int(np.amax(arcs[:, 0]))
 arcs[:, :2] = arcs[:, :2] - 1     # Correcting for the 0 index
-
 names = namesNodes.iloc[:, 0] + namesNodes.iloc[:, 6]
 nodes = arcs[:, :2]
 weights = arcs[:, 2]
 graph = [nodes[nodes[:, 0] == i, 1] for i in range(nbNodes)]
-
 w_matrix = np.zeros((len(weights), len(weights)))
 for idx, i, j in zip(range(len(arcs)), arcs[:, 0], arcs[:, 1]):
     w_matrix[int(i), int(j)] = arcs[idx, 2]
@@ -34,10 +32,13 @@ dist, prec = dijkstra(graph, w_matrix, originMode, destinationNode)
 print('Minimum distance from', names[originMode], 'to',
       names[destinationNode], '\n', dist[destinationNode])
 path = destinationNode
-step = 0
+pathList = [path]
 print('\n\nPath:')
 while path != originMode:
+    path = prec[path]
+    pathList.append(path)
+step = 0
+while pathList:
+    path = pathList.pop()
     print(step, names[path], '(#%d)' % (path + 1))
     step += 1
-    path = prec[path]
-print(step, names[path], '(#%d)' % (path + 1))
--- 02-appli-networks/subway/SPR_Paris.py
+++ 02-appli-networks/subway/SPR_Paris.py
@@ -13,11 +13,9 @@ namesNodes = pd.read_csv(thepath + "/Paris/nodes.csv", sep=',', header=None)
 # Preparing the data: setting up the graph and weight matrices
 nbNodes = int(np.amax(arcs[:, 0]))
 arcs[:, :-1] = arcs[:, :-1] - 1     # Correcting for the 0 index
-
 nodes = arcs[:, :-1]
 weights = arcs[:, 2]
 graph = [nodes[nodes[:, 0] == i, 1] for i in range(nbNodes)]
-
 w_matrix = np.zeros((len(weights), len(weights)))
 for idx, i, j in zip(range(len(arcs)), arcs[:, 0], arcs[:, 1]):
     w_matrix[int(i), int(j)] = arcs[idx, 2]
@@ -33,10 +31,13 @@ dist, prec = dijkstra(graph, w_matrix, originMode, destinationNode)
 print('Minimum distance from', namesNodes.iloc[originMode, 0], 'to',
       namesNodes.iloc[destinationNode, 0], '\n', dist[destinationNode])
 path = destinationNode
-step = 0
+pathList = [path]
 print('\n\nPath:')
 while path != originMode:
+    path = prec[path]
+    pathList.append(path)
+step = 0
+while pathList:
+    path = pathList.pop()
     print(step, namesNodes.iloc[path, 0], '(#%d)' % (path + 1))
     step += 1
-    path = prec[path]
-print(step, namesNodes.iloc[path, 0], '(#%d)' % (path + 1))
