559a94048c035ccc5903666281e4b34a9ae27a1b
gregory.dannay@sciencespo.fr <gregory.dannay@sciencespo.fr>
2018-05-20 11:02:25 +0200

    Paris

--- 02-appli-networks/subway/Paris/ParisSubway.py
+++ 02-appli-networks/subway/Paris/ParisSubway.py
@@ -2,6 +2,7 @@ import pandas as pd
 import gurobipy as grb
 import os
 import numpy as np
+import time
 from igraph import *
 
 
@@ -11,23 +12,24 @@ arcsData = pd.read_csv(thepath + "/arcs.csv", sep=';', header=None)
 namesNodes = pd.read_csv(thepath + "/nodes.csv", sep=',', header=None)
 
 
-# Preparing the data: setting up the graph and weight matrices
+# Preparing the data: nodes and weight matrices
 nbNodes = arcsData.iloc[:, 0].max()
 arcsList = [(i, j) for i, j in zip(arcsData.iloc[:, 0], arcsData.iloc[:, 1])]
-edges = [(i, j) for i, j in zip(arcsData.iloc[:, 0], arcsData.iloc[:, 1]) if i <= j]
-coordinates = [(i, -j) for i, j in zip(namesNodes.iloc[:, 2], namesNodes.iloc[:, 3])]
 weights = arcsData.iloc[:, 2]
 originNode = 84
 destinationNode = 116
 
+# Setting up the model
 m = grb.Model('subway')
 arcs = m.addVars(arcsList, obj=weights, name='arcs')
-m.addConstrs((arcs.sum('*', station) - arcs.sum(station, '*') == 0 for station in range(nbNodes) if station not in [originNode, destinationNode]))
-m.addConstr(arcs.sum('*', originNode) - arcs.sum(originNode, '*') == 1)
-m.addConstr(arcs.sum('*', destinationNode) - arcs.sum(destinationNode, '*') == -1)
+m.addConstrs((arcs.sum('*', station) - arcs.sum(station, '*') == 0 for station in range(nbNodes)
+              if station not in [originNode, destinationNode]), name='constr')
+m.addConstr(arcs.sum('*', originNode) - arcs.sum(originNode, '*') == 1, name='constr')
+m.addConstr(arcs.sum('*', destinationNode) - arcs.sum(destinationNode, '*') == -1, name='constr')
 
 m.optimize()
 
+# Display the solution
 path = originNode
 pathList = []
 if m.status == grb.GRB.Status.OPTIMAL:
@@ -43,7 +45,10 @@ if m.status == grb.GRB.Status.OPTIMAL:
                 path = arc[0]
                 pathList.append(path)
 
+# Setting up the graph and the visual style
 g = Graph()
+edges = [(i, j) for i, j in zip(arcsData.iloc[:, 0], arcsData.iloc[:, 1]) if i <= j]
+coordinates = [(i, -j) for i, j in zip(namesNodes.iloc[:, 2], namesNodes.iloc[:, 3])]
 g.add_vertices(nbNodes + 1)
 g.add_edges(edges)
 g.delete_vertices(0)
@@ -63,12 +68,16 @@ visual_style["vertex_label"] = [None for i in range(nbNodes) if i not in [origin
 visual_style["vertex_label"][originNode] = namesNodes.iloc[originNode - 1, 0]
 visual_style["vertex_label"][destinationNode] = namesNodes.iloc[destinationNode - 1, 0]
 plot(g, **visual_style)
+
+# Show the optimal path with iGraph
 path = originNode
-while path != destinationNode:
-    path = pathList.pop(0)
-    visual_style["vertex_size"][path] = 40
-    visual_style["vertex_label"][path] = namesNodes.iloc[path - 1, 0]
-    plot(g, **visual_style)
-    visual_style["vertex_size"][path] = 10
-    visual_style["vertex_label"][path] = None
+if m.status == grb.GRB.Status.OPTIMAL:
+    while path != destinationNode:
+        path = pathList.pop(0)
+        visual_style["vertex_size"][path] = 40
+        visual_style["vertex_label"][path] = namesNodes.iloc[path - 1, 0]
+        plot(g, **visual_style)
+        visual_style["vertex_size"][path] = 10
+        visual_style["vertex_label"][path] = None
+        #time.sleep(0.5)
 
